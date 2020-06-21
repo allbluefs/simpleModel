63870179000d3aca7410bbb3826be384f97a7087
fangsong <942644064@qq.com>
2020-06-21 10:55:46 +0800

    sql

--- src/main/resources/statics/model.sql
+++ src/main/resources/statics/model.sql
@@ -30,7 +30,7 @@ CREATE TABLE `sys_menu` (
   `icon` varchar(100) DEFAULT NULL,
   `order_num` int(4) DEFAULT NULL,
   PRIMARY KEY (`menu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_menu` */
 
@@ -51,7 +51,7 @@ CREATE TABLE `sys_role` (
 
 /*Data for the table `sys_role` */
 
-insert  into `sys_role`(`role_id`,`role_name`,`remark`,`dept_id`,`create_time`) values (1,'管理员','普通',NULL,'2020-06-19'),(2,'普通用户','吸血',NULL,'2020-06-19');
+insert  into `sys_role`(`role_id`,`role_name`,`remark`,`dept_id`,`create_time`) values (1,'管理员','普通',NULL,'2020-06-19'),(2,'学生','吸血',NULL,'2020-06-19');
 
 /*Table structure for table `sys_role_menu` */
 
@@ -62,11 +62,11 @@ CREATE TABLE `sys_role_menu` (
   `role_id` bigint(20) DEFAULT NULL COMMENT '角色id',
   `menu_id` bigint(20) DEFAULT NULL COMMENT '菜单id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_role_menu` */
 
-insert  into `sys_role_menu`(`id`,`role_id`,`menu_id`) values (8,1,0),(9,1,1),(10,1,2),(11,1,3),(12,1,4),(13,2,0),(14,2,1),(15,2,4);
+insert  into `sys_role_menu`(`id`,`role_id`,`menu_id`) values (8,1,0),(9,1,1),(10,1,2),(11,1,3),(12,1,4),(16,2,0),(17,2,6),(18,2,7);
 
 /*Table structure for table `sys_user` */
 
@@ -76,15 +76,20 @@ CREATE TABLE `sys_user` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `username` varchar(50) DEFAULT NULL COMMENT '账户',
   `password` varchar(50) DEFAULT NULL COMMENT '密码',
-  `user_type` tinyint(1) DEFAULT NULL COMMENT '用户类型（1普通用户 3管理员）',
   `chinese_name` varchar(20) DEFAULT NULL COMMENT '用户姓名',
+  `phone_number` varchar(100) DEFAULT NULL COMMENT '电话号码',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `id_card_number` varchar(100) DEFAULT NULL COMMENT '身份证号',
+  `qq` varchar(100) DEFAULT NULL COMMENT 'qq号',
+  `user_type` tinyint(1) DEFAULT NULL COMMENT '用户类型（1普通用户 3管理员）',
   `status` int(1) DEFAULT NULL COMMENT '1.激活',
+  `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `sys_user` */
 
-insert  into `sys_user`(`user_id`,`username`,`password`,`user_type`,`chinese_name`,`status`) values (1,'admin','admin',0,'管理员',1),(2,'Robin','123',1,'罗宾',1),(3,'lufei','123',1,'路飞',1);
+insert  into `sys_user`(`user_id`,`username`,`password`,`chinese_name`,`phone_number`,`email`,`id_card_number`,`qq`,`user_type`,`status`,`create_time`) values (1,'admin','admin','管理员','1','123213','123','123',0,1,NULL),(2,'Robin','123','罗宾','2','123','123','123',1,1,NULL),(3,'Luffy','123','路飞','3','123','123','123',1,1,NULL),(36,'Zoro','123','索隆','4','123','123','123',1,NULL,NULL),(37,'Sanji','123',NULL,'1231231','312313','1231231','3123123',1,NULL,'2020-06-21 09:46:34'),(38,'A','123','哈哈哈','12','213','123','123',1,NULL,'2020-06-21 10:53:49');
 
 /*Table structure for table `sys_user_role` */
 
@@ -95,30 +100,11 @@ CREATE TABLE `sys_user_role` (
   `user_id` bigint(20) NOT NULL,
   `role_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_user_role` */
 
-insert  into `sys_user_role`(`id`,`user_id`,`role_id`) values (1,1,1),(6,2,2),(7,3,2);
-
-/*Table structure for table `t_hotel` */
-
-DROP TABLE IF EXISTS `t_hotel`;
-
-CREATE TABLE `t_hotel` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL COMMENT '酒店名称',
-  `location` varchar(100) DEFAULT NULL COMMENT '酒店位置',
-  `room` int(4) DEFAULT NULL COMMENT '房间数',
-  `phone` varchar(100) DEFAULT NULL COMMENT '联系方式',
-  `user_id` bigint(20) DEFAULT NULL COMMENT '创建人',
-  `status` int(1) DEFAULT '0' COMMENT '0 未激活 1 激活',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
-/*Data for the table `t_hotel` */
-
-insert  into `t_hotel`(`id`,`name`,`location`,`room`,`phone`,`user_id`,`status`) values (1,'北京国际酒店','北京市朝阳区酒仙桥接到',4,'1662626262626',NULL,1),(3,'香格里拉','北京市朝阳区',4,'188687682314',NULL,1),(4,'罗宾的酒店','北京',100,'123123123',35,1),(5,'1','1',1,'1',1,1),(6,'1','1',1,'1',1,1),(7,'1','3',1,'1',1,1),(8,'1','2312',31,'1',1,1),(9,'3','3',3,'3',3,3),(10,'3','4',4,'44',4,4);
+insert  into `sys_user_role`(`id`,`user_id`,`role_id`) values (1,1,1),(6,2,2),(7,3,2),(8,36,2),(9,37,2),(10,38,2);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
